ddd8e3ada78973d9a13504d326e313f3ee006c6d
Anzor Balkar <abalkar@gmail.com>
2025-05-21 13:42:51 -0700

    Tiny Infra refactor.

--- src/ShelfLayoutManager.Infrastructure/InfrastructureExtensions.cs
+++ src/ShelfLayoutManager.Infrastructure/InfrastructureExtensions.cs
@@ -9,20 +9,15 @@ namespace ShelfLayoutManager.Infrastructure;
 public static class InfrastructureExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddDbContext<DbContext>(
-            options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-
-        services.AddScoped<ICabinetEntityConverter, CabinetEntityConverter>();
-        services.AddScoped<ICabinetsDataService, CabinetsDataService>();
-        services.AddScoped<IIngestionService, IngestionService>();
-        services.AddScoped<ILaneEntityConverter, LaneEntityConverter>();
-        services.AddScoped<IRowEntityConverter, RowEntityConverter>();
-        services.AddScoped<IShelfFileParser, ShelfFileParser>();
-        services.AddScoped<ISkuEntityConverter, SkuEntityConverter>();
-        services.AddScoped<ISkuFileParser, SkuFileParser>();
-        services.AddScoped<ISkusDataService, SkusDataService>();
-
-        return services;
-    }
+        => services
+            .AddDbContext<DbContext>(options => options.UseNpgsql(configuration.GetConnectionString("TxConnection")))
+            .AddScoped<ICabinetEntityConverter, CabinetEntityConverter>()
+            .AddScoped<ICabinetsDataService, CabinetsDataService>()
+            .AddScoped<IIngestionService, IngestionService>()
+            .AddScoped<ILaneEntityConverter, LaneEntityConverter>()
+            .AddScoped<IRowEntityConverter, RowEntityConverter>()
+            .AddScoped<IShelfFileParser, ShelfFileParser>()
+            .AddScoped<ISkuEntityConverter, SkuEntityConverter>()
+            .AddScoped<ISkuFileParser, SkuFileParser>()
+            .AddScoped<ISkusDataService, SkusDataService>();
 }
