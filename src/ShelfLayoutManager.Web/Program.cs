ff617fc42b6b768fac9f95b456c7003f939b1559
Anzor Balkar <abalkar@gmail.com>
2025-05-21 13:42:34 -0700

    Make Web project Azure friendly.

--- src/ShelfLayoutManager.Web/Program.cs
+++ src/ShelfLayoutManager.Web/Program.cs
@@ -2,32 +2,32 @@ using ShelfLayoutManager.Infrastructure;
 
 WebApplicationBuilder webAppBuilder = WebApplication.CreateBuilder(args);
 
+string allowClientAccessCorsPolicyName = "AllowClientAccess";
+string? clientBaseUrl = Environment.GetEnvironmentVariable("CLIENT_BASE_URL");
+
+if (string.IsNullOrEmpty(clientBaseUrl))
+{
+    // Local client; source:
+    // src/ShelfLayoutManager.Client/Properties/launchSettings.json, "https".
+    clientBaseUrl = "https://localhost:7152";
+}
+
 webAppBuilder.Services
     .AddInfrastructure(webAppBuilder.Configuration)
     .AddEndpointsApiExplorer()
     .AddSwaggerGen()
     .AddCors(options =>
-    {
-        options.AddPolicy("AllowBlazorLocalhost",
-            policy =>
-            {
-                policy.WithOrigins("https://localhost:7152")
-                    .AllowAnyMethod()
-                    .AllowAnyHeader();
-            });
-    })
+        options.AddPolicy(
+            allowClientAccessCorsPolicyName,
+            policy => policy.WithOrigins(clientBaseUrl).AllowAnyMethod().AllowAnyHeader()))
     .AddControllers();
 
 WebApplication webApp = webAppBuilder.Build();
 
-if (webApp.Environment.IsDevelopment())
-{
-    webApp.UseSwagger();
-    webApp.UseSwaggerUI(options => options.EnableTryItOutByDefault());
-}
-
-webApp.UseHttpsRedirection().UseCors("AllowBlazorLocalhost").UseAuthorization();
-
+webApp.UseSwagger();
+webApp.UseSwaggerUI(options => options.EnableTryItOutByDefault());
+webApp.UseHttpsRedirection();
+webApp.UseCors(allowClientAccessCorsPolicyName);
 webApp.MapControllers();
 
 webApp.Run();
